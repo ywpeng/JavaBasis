e6cb31d3742caa6f2e0d2dc71fbb4d23d51cda0c
pengyangweicx <pengyangwei@ccxe.com.cn>
2020-11-10 18:03:26 +0800

    pair 1.添加对配对类型的数据操作的示例代码

---
+++ src/com/pyw/base/map/PairTest.java
@@ -0,0 +1,24 @@
+package com.pyw.base.map;
+
+import javafx.util.Pair;
+
+/**
+ * @author pengyangwei
+ * @create 2020-11-10 17:49
+ */
+public class PairTest {
+
+    public static void main(String[] args) {
+        /*
+         * 在javax.util包下，有一个简单Pair类可以直接调用，用法是直接通过构造函数将所吸引类型的Key和value存入，这个key和value没有任何的对应关系类型也是任意定的。
+         */
+        Pair<String,String>  namePair = new Pair<>("张三","zs");
+        String pk = namePair.getKey();
+        String pn = namePair.getValue();
+        String ps = namePair.toString();
+        System.out.println(pk);
+        System.out.println(pn);
+        System.out.println(ps);
+
+    }
+}
