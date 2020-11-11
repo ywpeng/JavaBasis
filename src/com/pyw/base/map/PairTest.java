7c448db70b485e9c33fe3873fd22259655e94b06
pengyangweicx <pengyangwei@ccxe.com.cn>
2020-11-11 14:51:06 +0800

    pair 1.添加对配对类型的数据操作的示例代码

--- src/com/pyw/base/map/PairTest.java
+++ src/com/pyw/base/map/PairTest.java
@@ -21,6 +21,6 @@ public class PairTest {
         System.out.println(pk);
         System.out.println(pn);
         System.out.println(ps);
-        System.out.println("12345");
+        System.out.println("123456");
     }
 }
